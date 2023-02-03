6f2c8927a533e43fdd555536d24de97d3862b454
grant8827 <grant8827@yahoo.com>
2023-02-02 22:35:19 -0500

    js css added to slide

--- js/main.js
+++ js/main.js
@@ -21,9 +21,8 @@ dropDown.addEventListener("click", function(slow) {
 
 
 
-//slide
-let slideIndex = 1;
-showSlides(slideIndex);
+//slide click slide
+
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -46,7 +45,28 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
     dots[slideIndex - 1].className += " active";
+}
+//auto slide
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
 }
 
 //login form
